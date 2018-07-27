6b95ac94a72a021c87d0521dc8c73c97d0cbf534
LILIT <lilit555ai@gmail.com>
2018-07-26 22:59:56 -0600

    Add classifier function

--- problem3_3.py
+++ problem3_3.py
@@ -9,6 +9,26 @@ cv = StratifiedShuffleSplit(n_splits = 5, test_size = 0.4, random_state = 42)
 results = [['svm_linear'], ['svm_polynomial'], ['svm_rbf'], ['logistic'],\
            ['knn'], ['decision_tree'], ['random_forest']]
 
+def classifier(estimator, parameters, cv, X, y):
+    clf = GridSearchCV(estimator, parameters, cv = cv, return_train_score = True )
+    clf.fit(X, y)
+    scores = clf.cv_results_
+    max_train = 0
+    max_test = 0
+    for i in range(5):
+        train = 'split' + str(i) + '_train_score'
+        test = 'split' + str(i) + '_test_score'
+        if max(scores[train]) > max_train:
+            max_train = max(scores[train])
+        if max(scores[test]) > max_test:
+            max_test = max(scores[test])
+    
+    return [max_train, max_test]
+
+
+
+
+
 
                         
 
